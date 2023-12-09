8a15eda5f4b0dee31d80b5ccc0cb61ea4b47e108
Andrew <shakeyshakespeare0@gmail.com>
2023-12-09 12:20:25 +0000

    Log in and sign up boxes

--- Games0App/static/script.js
+++ Games0App/static/script.js
@@ -1,23 +1,72 @@
 document.addEventListener('DOMContentLoaded', function() {
+
+    const openBoxButtonsOver = document.querySelectorAll('[data-login-button-target], [data-register-button-target]');
+    const cancelBoxButtonsOver = document.querySelectorAll('[data-cancel-button-over]');
+    const overlay = document.getElementById('overlay');
+
+    openBoxButtonsOver.forEach(button => {
+        button.addEventListener('click', () => {
+            const boxSelector = button.dataset.loginButtonTarget || button.dataset.registerButtonTarget;
+            const box = document.querySelector(boxSelector);
+            openBoxOver(box);
+        });
+    });
+
+    cancelBoxButtonsOver.forEach(button => {
+        button.addEventListener('click', () => {
+            const box = button.closest('.login-box, .register-box');
+            closeBoxOver(box);
+        });
+    });
+
+    function openBoxOver(box) {
+        if (box == null) return;
+        box.classList.add('active');
+        overlay.classList.add('active');
+        toggleScrollLock(true);
+    }
+
+    function closeBoxOver(box) {
+        if (box == null) return;
+        box.classList.remove('active');
+        overlay.classList.remove('active');
+        toggleScrollLock(false);
+    }
+
+    function toggleScrollLock(isLocked) {
+        const body = document.body;
+    
+        if (isLocked) {
+            const scrollY = window.scrollY;
+            body.style.position = 'fixed';
+            body.style.top = `-${scrollY}px`;
+            body.style.width = '100%';
+        } else {
+            const scrollY = body.style.top;
+            body.style.position = '';
+            body.style.top = '';
+            window.scrollTo(0, parseInt(scrollY || '0') * -1);
+        }
+    }
+
     var menuButton = document.querySelector('.menu-button');
     var optionsMenu = document.getElementById('options-menu');
-    var timeoutId;
+    var isMenuVisible = false;
 
-    function showMenu() {
-        clearTimeout(timeoutId);
-        optionsMenu.style.display = 'flex';
+    function toggleMenu() {
+        isMenuVisible = !isMenuVisible;
+        optionsMenu.style.display = isMenuVisible ? 'flex' : 'none';
+        menuButton.style.color = isMenuVisible ? 'rgb(251, 223, 158)' : 'rgb(232, 213, 203)';
+    }
+    function handleClickOutside(event) {
+        if (isMenuVisible && !optionsMenu.contains(event.target) && !menuButton.contains(event.target)) {
+            toggleMenu();
+        }
     }
 
-    function hideMenu() {
-        timeoutId = setTimeout(function() {
-            optionsMenu.style.display = 'none';
-        }, 250);
-    }
+    menuButton.addEventListener('click', toggleMenu);
+    document.addEventListener('click', handleClickOutside);
 
-    menuButton.addEventListener('mouseenter', showMenu);
-    optionsMenu.addEventListener('mouseenter', showMenu);
-    menuButton.addEventListener('mouseleave', hideMenu);
-    optionsMenu.addEventListener('mouseleave', hideMenu);
 });
 
 document.addEventListener('DOMContentLoaded', (event) => {
