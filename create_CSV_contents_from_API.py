dfcf72d900dabf122cccf95302e50b6a09139bcd
Andrew <shakeyshakespeare0@gmail.com>
2024-01-11 23:20:38 +0000

    More jokes

--- create_CSV_contents_from_API.py
+++ create_CSV_contents_from_API.py
@@ -6,8 +6,8 @@ import requests
 import csv
 
 
-def get_last_id():
-    with open('Games0App/static/jokes.csv', 'r') as f:
+def get_last_id(word):
+    with open(f'Games0App/static/{word}s.csv', 'r') as f:
         reader = csv.reader(f, delimiter=';')
         last_id = 0
         for row in reader:
@@ -15,55 +15,89 @@ def get_last_id():
                 last_id = int(row[0])
     return last_id
 
-last_id = get_last_id()
-print("Last ID:", last_id)
-write_questions = input("Do you want to write questions to the CSV? (y/n) ")
-if write_questions.lower() == 'y':
-    API_KEY = os.environ.get('NINJA_API_KEY')
-    # response = requests.get("https://api.api-ninjas.com/v1/jokes?limit=30", headers={'X-Api-Key': API_KEY})
-    response = requests.get('https://official-joke-api.appspot.com/jokes/ten', headers={'X-Api-Key': API_KEY})
-    response = json.loads(response.text)
-    print(response)
+print("1. Add jokes from API Ninjas to CSV")
+print("2. Add jokes from Official Joke API to CSV")
+print("3. Add facts from API Ninjas to CSV")
+write_questions = input("Choose the number of your CSV and API: ")
+if write_questions == '1':
+    api = 'https://api.api-ninjas.com/v1/jokes?limit=30'
+    word = 'joke'
+elif write_questions == '2':
+    api = 'https://official-joke-api.appspot.com/jokes/ten'
+    word = 'joke'
+elif write_questions == '3':
+    api = 'https://api.api-ninjas.com/v1/facts?limit=30'
+    word = 'fact'
+else:
+    print("Invalid input.")
+    exit()
 
-    for item in response:
-        os.system('clear')
-        # if len(item['joke']) > 100 or '?' not in item['joke'] or len(item['joke'].split('? ')) < 2:
-        #     continue
-        # joke_question = item['joke'].split('? ')[0] + '?'
-        # joke_answer = item['joke'].split('? ')[1]
-        joke_question = item['setup']
-        joke_answer = item['punchline']
-        is_okay = input(f"Is this joke okay? (y/n)\nJOKE QUESTION: {joke_question}\nJOKE ANSWER: {joke_answer}\n")
-        if is_okay.lower() == 'y':
-            blank_words = []
-            while True:
-                print('Blank words so far:', blank_words)
-                blank_word = input("Enter a word that can be a blank or Q to stop adding: ")
-                if blank_word.lower() == 'q':
-                    break
-                elif len(blank_word) >= 3:
-                    blank_words.append(blank_word)
-                else:
-                    print("Word too short.")
-        else:
+last_id = get_last_id(word)
+API_KEY = os.environ.get('NINJA_API_KEY')
+if write_questions == '1':
+    response = requests.get("https://api.api-ninjas.com/v1/jokes?limit=30", headers={'X-Api-Key': API_KEY})
+elif write_questions == '2':
+    response = requests.get('https://official-joke-api.appspot.com/jokes/ten')
+elif write_questions == '3':
+    response = requests.get('https://api.api-ninjas.com/v1/facts?limit=30', headers={'X-Api-Key': API_KEY})
+response = json.loads(response.text)
+print(response)
+
+for item in response:
+    os.system('clear')
+    if write_questions == '1':
+        if len(item['joke']) > 100 or '?' not in item['joke'] or len(item['joke'].split('? ')) < 2:
             continue
-        supposed_id = last_id + 1
-        joke_to_add = {"id": supposed_id, "joke": joke_question, "answer": joke_answer, "blanks": blank_words}
-        os.system('clear')
-        print("FINAL JOKE:", joke_to_add)
-        abandon = False
+        main_part = item['joke'].split('? ')[0] + '?'
+        answer_part = item['joke'].split('? ')[1]
+    elif write_questions == '2':
+        main_part = item['setup']
+        answer_part = item['punchline']
+    elif write_questions == '3':
+        if len(item['fact']) > 100:
+            continue
+        main_part = item['fact']
+        answer_part = 'NONE'
+    is_okay = input(f"Is this {word} okay? (y/n)\n{word.upper()} QUESTION: {main_part}\n{word.upper()} ANSWER: {answer_part}\n")
+    if is_okay.lower() == 'y':
+        blank_words = []
         while True:
-            is_okay = input("Add joke to CSV? (y/n) ")
-            if is_okay.lower() == 'y':
+            print('Blank words so far:', blank_words)
+            blank_word = input("Enter a word that can be a blank or Q to stop adding: ")
+            if blank_word.lower() == 'q':
                 break
-            elif is_okay.lower() == 'n':
-                abandon = True
-                break
-        if not abandon:
-            last_id += 1
+            elif len(blank_word) >= 3:
+                blank_words.append(blank_word)
+            else:
+                print("Word too short.")
+    else:
+        continue
+    supposed_id = last_id + 1
+    if word == 'joke':
+        entry_to_add = {"id": supposed_id, "joke": main_part, "answer": answer_part, "blanks": blank_words}
+    elif word == 'fact':
+        entry_to_add = {"id": supposed_id, "fact": main_part, "blanks": blank_words}
+    os.system('clear')
+    print(f"FINAL {word.upper()}:", entry_to_add)
+    abandon = False
+    while True:
+        is_okay = input(f"Add {word} to CSV? (y/n) ")
+        if is_okay.lower() == 'y':
+            break
+        elif is_okay.lower() == 'n':
+            abandon = True
+            break
+    if not abandon:
+        last_id += 1
+        if word == 'joke':
             output_file_path = 'Games0App/static/jokes.csv'
-            with open(output_file_path, 'a', newline='') as csvfile:
-                writer = csv.writer(csvfile, delimiter=';')
-                writer.writerow([str(joke_to_add["id"]), joke_to_add['joke'], joke_to_add['answer'],
-                                ', '.join(joke_to_add['blanks'])])
-            print("Joke added to CSV.")
+        elif word == 'fact':
+            output_file_path = 'Games0App/static/facts.csv'
+        with open(output_file_path, 'a', newline='') as csvfile:
+            writer = csv.writer(csvfile, delimiter=';')
+            if word == 'joke':
+                writer.writerow([str(entry_to_add["id"]), entry_to_add['joke'], entry_to_add['answer'],
+                                ', '.join(entry_to_add['blanks'])])
+            elif word == 'fact':
+                writer.writerow([str(entry_to_add["id"]), entry_to_add['fact'], ', '.join(entry_to_add['blanks'])])
+        print("Joke added to CSV.")
