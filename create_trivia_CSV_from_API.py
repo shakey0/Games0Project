7994f285dfbdf5638f14863b51a8d5e02893e8d1
Andrew <shakeyshakespeare0@gmail.com>
2024-01-14 12:12:24 +0000

    More trivia questions

--- create_trivia_CSV_from_API.py
+++ create_trivia_CSV_from_API.py
@@ -16,10 +16,13 @@ def get_last_id():
                 last_id = int(row[0])
     return last_id
 
-print("1. Add jokes from API Ninjas to CSV")
+print("1. Add jokes from Open Trivia Database to CSV")
+print("2. Add jokes from API Ninjas to CSV")
 write_questions = input("Choose the number of your CSV and API: ")
 if write_questions == '1':
     api = 'https://opentdb.com/api.php?amount=50&type=multiple'
+elif write_questions == '2':
+    api = 'https://api.api-ninjas.com/v1/trivia?limit=30'
 else:
     print("Invalid input.")
     exit()
@@ -27,35 +30,51 @@ else:
 last_id = get_last_id()
 if write_questions == '1':
     response = requests.get(api)
+elif write_questions == '2':
+    API_KEY = os.environ.get('NINJA_API_KEY')
+    response = requests.get(api, headers={'X-Api-Key': API_KEY})
 response = json.loads(response.text)
 print(response)
+if write_questions == '1':
+    response = response['results']
 
-for item in response["results"]:
+for item in response:
 
-    os.system('clear')
-
-    category = html.unescape(item['category'].lower())
-    if not category:
-        category = 'all'
-    if ": " in category:
-        category = category.split(": ")[1]
-    if " & " in category:
-        category = category.replace(" & ", "_and_")
-    if " " in category:
-        category = category.replace(" ", "_")
-    question = html.unescape(item['question'].strip())
-    answer = item['correct_answer'].strip()
+    if write_questions == '1':
+        category = html.unescape(item['category'].lower())
+        if not category:
+            category = 'all'
+        if ": " in category:
+            category = category.split(": ")[1]
+        if " & " in category:
+            category = category.replace(" & ", "_and_")
+        if " " in category:
+            category = category.replace(" ", "_")
+        question = html.unescape(item['question'].strip())
+        answer = html.unescape(item['correct_answer'].strip())
+    
+    elif write_questions == '2':
+        category = item['category']
+        question = item['question'].strip()
+        if question[-1] != '?':
+            question += '?'
+        if not question[0].isupper():
+            question = question[0].upper() + question[1:]
+        answer = item['answer']
     
     other_answers = []
     while True:
+        os.system('clear')
         print()
         print('CATEGORY:', category)
         print('QUESTION:', question)
         print('ANSWER:', answer)
         print('OTHER ANSWERS:', other_answers)
         print()
-        choice = input('Enter=add, A=edit_answer, Q=edit_question, Z=discard, O=add_other_answer: ')
-        if choice.lower() == 'a':
+        choice = input('Enter=add, C=edit_category, A=edit_answer, Q=edit_question, Z=discard, O=add_other_answer: ')
+        if choice.lower() == 'c':
+            category = input('Enter category: ')
+        elif choice.lower() == 'a':
             answer = input('Enter answer: ')
         elif choice.lower() == 'q':
             question = input('Enter question: ')
