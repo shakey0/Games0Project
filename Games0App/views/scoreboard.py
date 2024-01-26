087bf1b406ed20b646afdd9b85e4aa25e9e8522a
Andrew <shakeyshakespeare0@gmail.com>
2024-01-26 16:54:00 +0000

    Account management styling done

--- Games0App/views/scoreboard.py
+++ Games0App/views/scoreboard.py
@@ -67,7 +67,7 @@ def scoreboard_page():
         high_scores = get_high_scores(game_name_param)
 
     elif username:
-        game_name = f"{username}'s Scores"
+        game_name = f"{username}'s Scores" if username != current_user.username else "Your Profile & Scores"
 
         needs_high_score = False
 
@@ -88,6 +88,7 @@ def scoreboard_page():
 
 
 @scoreboard.route('/amend_score', methods=['POST'])
+@login_required
 def amend_score():
 
     score_id = request.form.get('score_id')
