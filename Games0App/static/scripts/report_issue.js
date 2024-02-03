96217737a95eb7325534f2fc4df0194203928f06
Andrew <shakeyshakespeare0@gmail.com>
2024-02-03 11:58:04 +0000

    Report issue functionality

---
+++ Games0App/static/scripts/report_issue.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    var form = document.querySelector('form[action="/report_issue"]');
+    var errorMessage = document.querySelector('.error-message');
+
+    form.addEventListener('submit', function(event) {
+        var issueIdInput = document.getElementById('issue_id');
+        var issueDescription = document.getElementById('issue-description').value.trim();
+
+        var issueIdExistsAndEmpty = issueIdInput && !issueIdInput.value.trim();
+        var issueDescriptionEmpty = !issueDescription;
+
+        if (issueIdExistsAndEmpty && issueDescriptionEmpty) {
+            event.preventDefault();
+            errorMessage.style.display = 'block';
+            errorMessage.textContent = 'Please include a case number or a description.';
+            return false;
+        }
+    });
+
+    const issueId = document.querySelectorAll('.issue_id');
+
+    issueId.forEach(function(field) {
+        field.addEventListener('input', function() {
+            this.value = this.value.toUpperCase();
+        });
+    });
+
+});
