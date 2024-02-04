9113fe74b34e5960745cbe661e0e2a7f6bc9aef6
Andrew <shakeyshakespeare0@gmail.com>
2024-02-04 19:14:02 +0000

    CITEXT added for emails

--- Games0App/models/user.py
+++ Games0App/models/user.py
@@ -1,13 +1,12 @@
 from Games0App import db
-from sqlalchemy.dialects.postgresql import JSONB
-from sqlalchemy.dialects.postgresql import BYTEA
+from sqlalchemy.dialects.postgresql import JSONB, BYTEA, CITEXT
 from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
+    email = db.Column(CITEXT, index=True, unique=True)
     password_hashed = db.Column(BYTEA)
     last_50_questions = db.Column(JSONB)
 
