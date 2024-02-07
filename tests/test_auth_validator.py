6a994f22baa7a6ac638febdf722dda56d74402c3
Andrew <shakeyshakespeare0@gmail.com>
2024-02-07 17:39:08 +0000

    AuthValidator tests tidied

--- tests/test_auth_validator.py
+++ tests/test_auth_validator.py
@@ -6,33 +6,53 @@ from Games0App.models.email_log import EmailLog
 from Games0App.classes.auth_validator import auth_validator
 import os
 
+
 def test_validate_password_for_auth(test_app):
     
     os.environ['FLASK_ENV'] = 'testing'
     app = create_app()
     with app.app_context(), app.test_request_context():
         with patch('Games0App.classes.auth_validator.request') as mock_request, \
-                patch('Games0App.classes.auth_validator.current_user') as mock_current_user, \
-                patch('Games0App.classes.auth_token_manager.current_user') as mock_current_user_2:
+            patch('Games0App.classes.auth_validator.current_user') as mock_current_user, \
+            patch('Games0App.classes.auth_token_manager.current_user') as mock_current_user_2:
             
-            redis_client.flushall()
-            mock_request.form.get = MagicMock(return_value='password')
+            redis_client.flushall() # Clear Redis cache
             mock_current_user.id = 1
+            mock_current_user_2.id = 1
+            mock_current_user.username = 'username'
+            mock_current_user.email = 'user@example.com'
             mock_current_user.password_hashed = b'$2b$12$/yCwu3uKdkdzKKN/ek.KCOJpdyrMhVxXyFI.39havwIpCFKWD4YI6'
+
+            # Test the function with a correct password
+            mock_request.form.get = MagicMock(return_value='password')
             assert auth_validator.validate_password_for_auth() == True # First attempt
+
+            # Test the function with a blank password
             mock_request.form.get = MagicMock(return_value='')
             assert auth_validator.validate_password_for_auth() == "Please enter your password." # Bypasses attempt check
-            mock_current_user_2.id = 1
+            
+            # Test the function with a wrong password
             mock_request.form.get = MagicMock(return_value='wrong_password')
             assert auth_validator.validate_password_for_auth() == "Something didn't match! Please try again." # Second attempt
-            redis_client.flushall()
+            
+            redis_client.flushall() # Clear Redis cache
+
+            # Test the function with a wrong password 2 twice and then the correct password
             for _ in range(2):
                 assert auth_validator.validate_password_for_auth() == "Something didn't match! Please try again."
-            mock_current_user.email = 'user@example.com'
-            mock_current_user.username = 'username'
+            mock_request.form.get = MagicMock(return_value='password')
+            assert auth_validator.validate_password_for_auth() == True # Third attempt
+
+            redis_client.flushall() # Clear Redis cache
+
+            # Test the function with a wrong password 13 times
+            mock_request.form.get = MagicMock(return_value='wrong_password')
+            for _ in range(2):
+                assert auth_validator.validate_password_for_auth() == "Something didn't match! Please try again."
             for _ in range(11):
                 assert auth_validator.validate_password_for_auth() == "This is not good! You'll have to wait 10 minutes."
             
+            # Check the log has been created in the database
             logs = Log.query.all()
             assert len(logs) == 1
             assert logs[0].id == 1
@@ -45,6 +65,7 @@ def test_validate_password_for_auth(test_app):
             assert not logs[0].data
             assert not logs[0].issue_id
 
+            # Check the email log has been created in the database
             email_logs = EmailLog.query.all()
             assert len(email_logs) == 1
             assert email_logs[0].id == 1
@@ -57,78 +78,129 @@ def test_validate_password_for_auth(test_app):
             assert email_logs[0].json_response == {'success': True}
             assert email_logs[0].timestamp != None
 
+
 def test_validate_new_user_name(test_app):
+
     os.environ['FLASK_ENV'] = 'testing'
     app = create_app()
     with app.app_context(), app.test_request_context():
         with patch('Games0App.classes.auth_validator.request') as mock_request:
+
+            # Test the function with a valid username
             mock_request.form.get = MagicMock(return_value='test')
             assert auth_validator.validate_new_user_name() == True
+
+            # Test the function with an empty username
             mock_request.form.get = MagicMock(return_value='')
             assert auth_validator.validate_new_user_name() == 'Please enter a username.'
+
+            # Test the function with a username that is too short
             mock_request.form.get = MagicMock(return_value='a')
             assert auth_validator.validate_new_user_name() == 'Username must be at least 4 characters.'
+
+            # Test the function with a username that is too long
             mock_request.form.get = MagicMock(return_value='a'*21)
             assert auth_validator.validate_new_user_name() == 'Username must be max 20 characters.'
+
+            # Test the function with a username that contains invalid characters
             mock_request.form.get = MagicMock(return_value='test!')
             assert auth_validator.validate_new_user_name() == 'Username must only contain letters, numbers, underscores and hyphens.'
 
+
 def test_validate_new_email(test_app):
+
     os.environ['FLASK_ENV'] = 'testing'
     app = create_app()
     with app.app_context(), app.test_request_context():
         with patch('Games0App.classes.auth_validator.request') as mock_request:
+
+            # Test the function with a valid email
             mock_request.form.get = MagicMock(return_value='test@test.com')
             assert auth_validator.validate_new_email() == True
+
+            # Test the function with an empty email
             mock_request.form.get = MagicMock(return_value='')
             assert auth_validator.validate_new_email() == 'Please enter an email.'
+
+            # Test the function with an email that is too short
             mock_request.form.get = MagicMock(return_value='a')
             assert auth_validator.validate_new_email() == 'Email must be at least 3 characters.'
+
+            # Test the function with an email that does not contain an @ symbol
             mock_request.form.get = MagicMock(return_value='test.com')
             assert auth_validator.validate_new_email() == 'Please enter a valid email.'
+
+            # Test the function with an email that contains 2 @ symbols
             mock_request.form.get = MagicMock(return_value='test@@test.com')
             assert auth_validator.validate_new_email() == 'Please enter a valid email.'
+
+            # Test the function with an email that contains a space
             mock_request.form.get = MagicMock(return_value='test test@test.com')
             assert auth_validator.validate_new_email() == 'Please enter a valid email.'
 
+
 def test_validate_new_password(test_app):
+
     os.environ['FLASK_ENV'] = 'testing'
     app = create_app()
     with app.app_context(), app.test_request_context():
+
         def custom_form_get_1(key):
             if key == 'password':
                 return 'validPassword'
             elif key == 'confirm_password':
                 return ''
             return None
+        
         def custom_form_get_2(key):
             if key == 'password':
                 return 'validPassword'
             elif key == 'confirm_password':
                 return 'differentPassword'
             return None
+        
         with patch('Games0App.classes.auth_validator.request') as mock_request:
+
+            # Test the function with a valid password
             mock_request.form.get = MagicMock(return_value='validPassword')
             assert auth_validator.validate_new_password() == True
+
+            # Test the function with an empty password
             mock_request.form.get = MagicMock(return_value='')
             assert auth_validator.validate_new_password() == ('Please enter a password.', 'password')
+
+            # Test the function with a password that is too short
             mock_request.form.get = MagicMock(return_value='a'*7)
             assert auth_validator.validate_new_password() == ('Password must be at least 8 characters.', 'password')
+
+            # Test the function with an empty confirm password
             mock_request.form.get = MagicMock(side_effect=custom_form_get_1)
             assert auth_validator.validate_new_password() == ('Please confirm your password.', 'confirm_password')
+
+            # Test the function with a password that does not match the confirm password
             mock_request.form.get = MagicMock(side_effect=custom_form_get_2)
             assert auth_validator.validate_new_password() == ('Passwords do not match.', 'confirm_password')
 
+
 def test_validate_victory_message(test_app):
+
     os.environ['FLASK_ENV'] = 'testing'
     app = create_app()
     with app.app_context(), app.test_request_context():
         with patch('Games0App.classes.auth_validator.request') as mock_request:
+
+            # Test the function with a valid victory message
             mock_request.form.get = MagicMock(return_value='test')
             assert auth_validator.validate_victory_message() == True
+
+            # Test the function with an empty victory message (this is allowed)
             mock_request.form.get = MagicMock(return_value='')
             assert auth_validator.validate_victory_message() == True
+
+            # Test the function with a victory message that is too long
             mock_request.form.get = MagicMock(return_value='a'*26)
             assert auth_validator.validate_victory_message() == "Max 25 characters."
+
+            # Test the function with a victory message that contains bad language
             mock_request.form.get = MagicMock(return_value='Shit!') # I sincerely apologise for this
             assert auth_validator.validate_victory_message() == "Contains bad language."
