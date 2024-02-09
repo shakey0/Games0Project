da404c6a2c3d936c3439eec4d33b5be389db002a
Andrew <shakeyshakespeare0@gmail.com>
2024-02-09 00:33:17 +0000

    Testing game_play route

--- tests/test_main_routes.py
+++ tests/test_main_routes.py
@@ -1,8 +1,9 @@
 from playwright.sync_api import expect
 from Games0App.extensions import db, redis_client
+import json
 
 
-def test_index_route(page, flask_server):
+def test_index_route(page, flask_server, test_app):
     page.goto("http://localhost:5000/")
     page.click("text='Continue to Website'")
     title = page.locator("h1")
@@ -15,7 +16,7 @@ def test_index_route(page, flask_server):
     ])
 
 
-def test_game_setup_route(page, flask_server):
+def test_game_setup_route(page, flask_server, test_app):
     page.goto("http://localhost:5000/")
     page.click("text='Continue to Website'")
     page.dispatch_event(".t-fill_blank_facts", "click")
@@ -26,9 +27,11 @@ def test_game_setup_route(page, flask_server):
     start_game_button = page.locator(".submit-game-btn")
     expect(start_game_button).to_have_text("Start Game")
     page.dispatch_event(".submit-game-btn", "click")
+    game_title = page.locator(".game-title-tag")
+    expect(game_title).to_have_text("Fill in the Blank - Facts")
 
 
-def test_game_setup_route_with_categories(page, flask_server):
+def test_game_setup_route_with_categories(page, flask_server, test_app):
     page.goto("http://localhost:5000/")
     page.click("text='Continue to Website'")
     page.dispatch_event(".t-trivia_madness_categories", "click")
@@ -38,9 +41,11 @@ def test_game_setup_route_with_categories(page, flask_server):
     title = page.locator("h1")
     expect(title).to_have_text('Trivia Madness - Showbiz - HARD')
     page.dispatch_event(".submit-game-btn", "click")
+    game_title = page.locator(".game-title-tag")
+    expect(game_title).to_have_text("Trivia Madness - Showbiz - HARD")
     
 
-def test_game_setup_route_with_difficulty(page, flask_server):
+def test_game_setup_route_with_difficulty(page, flask_server, test_app):
     page.goto("http://localhost:5000/")
     page.click("text='Continue to Website'")
     page.dispatch_event(".t-trivia_mc_categories", "click")
@@ -52,15 +57,22 @@ def test_game_setup_route_with_difficulty(page, flask_server):
     page.dispatch_event("#easy", "click")
     page.dispatch_event("#hard2", "click")
     page.dispatch_event(".submit-game-btn", "click")
+    game_title = page.locator(".game-title-tag")
+    expect(game_title).to_have_text("Trivia - Multiple Choice - Science")
 
 
-def test_game_play_route(page, flask_server):
-    import os
+def test_game_play_route(page, flask_server, test_app):
     redis_client.flushall()
     page.goto("http://localhost:5000/")
     page.click("text='Continue to Website'")
     page.dispatch_event(".t-fill_blank_jokes", "click")
     page.dispatch_event("#hard", "click")
     page.dispatch_event(".submit-game-btn", "click")
-    page.wait_for_timeout(5000)
-    page.screenshot(path="tests/screenshots/capture.png")
+    game_title = page.locator(".game-title-tag")
+    expect(game_title).to_have_text("Fill in the Blank - Jokes")
+    # page.screenshot(path="tests/screenshots/capture.png")
+    question_value = redis_client.hget("fillintheblankjokes_hash", "fillintheblankjokes_1")
+    # print(question_value)
+    question = json.loads(question_value.decode("utf-8"))
+    question_text = page.locator(".func-in")
+    expect(question_text).to_have_text(question[1])
