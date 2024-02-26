340c03df9bf52260364d682ffad0dea3064183a1
Andrew <shakeyshakespeare0@gmail.com>
2024-02-26 14:50:38 +0000

    print db connection string

--- run.py
+++ run.py
@@ -7,6 +7,7 @@ app = create_app()
 import os
 state = os.environ.get('FLASK_ENV', 'development')
 if __name__ == '__main__':
+    print(os.environ['SQLALCHEMY_DATABASE_URI'])
     if state == 'production':
         app.run(host='0.0.0.0', debug=False, port=int(os.environ.get('PORT', 5000)))
     elif state == 'testing' or state == 'testing_in_actions':
